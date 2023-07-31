799cba8e4c15ea18762e88093669fb5f23c8a4d9
MichalStabrawa <mistaaqua@gmail.com>
2023-07-31 20:05:00 +0200

    format code ,add new fill in sanity backend

--- sanity-backend/schemas/pet.js
+++ sanity-backend/schemas/pet.js
@@ -1,24 +1,29 @@
 // schemas/pet.js
 export default {
-    name: 'pet',
-    type: 'document',
-      title: 'Pet',
-    fields: [
-      {
-        name: 'name',
-        type: 'string',
-        title: 'Name'
-      },
-      {
-        name: 'Number',
-        type: 'number',
-        title: 'number'
-      },
-      {
-        name: 'Image',
-        type: 'image',
-        title: 'photo'
-      },
-      
-    ]
-  }
+  name: 'pet',
+  type: 'document',
+  title: 'Pet',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Name'
+    },
+    {
+      name: 'Number',
+      type: 'number',
+      title: 'number'
+    },
+    {
+      name: 'Image',
+      type: 'image',
+      title: 'photo'
+    },
+    {
+      name: 'Margeting',
+      type: 'string',
+      title: 'marketing'
+    }
+
+  ]
+}
