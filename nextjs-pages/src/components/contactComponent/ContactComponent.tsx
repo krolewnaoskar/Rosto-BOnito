b3d80534d31d960f86caefc718dc60dad44651bd
Michal Stabrawa <mistaaqua@gmail.com>
2023-10-24 23:06:24 +0200

    add custom styles pink icon

--- nextjs-pages/src/components/contactComponent/ContactComponent.tsx
+++ nextjs-pages/src/components/contactComponent/ContactComponent.tsx
@@ -1,18 +1,26 @@
 import React, { useState } from "react";
 import styles from "./ContactComponent.module.scss";
 
-import {FiMapPin,FiPhone} from 'react-icons/fi'
-import {MdAlternateEmail} from 'react-icons/md'
+import { FiMapPin, FiPhone } from "react-icons/fi";
+import { MdAlternateEmail } from "react-icons/md";
 
 const ContactComponent = (props: any) => {
   const [state, setState] = useState(props.contact[0]);
-  console.log('KONTAKT DANE')
-  console.log(state)
+  console.log("KONTAKT DANE");
+  console.log(state);
   return (
     <div className={styles.contact}>
-        <p><FiPhone fontSize={36}/> <span className="phone">{state.phone}</span></p>
-      <FiMapPin font-Size={36}/> <span>{state.city} {state.street}</span>
-      <p><MdAlternateEmail font-size={36}/></p>
+      <p>
+        <FiPhone fontSize={36} className="icon_pink" />{" "}
+        <span className="phone">{state.phone}</span>
+      </p>
+      <FiMapPin font-Size={36} className="icon_pink" />{" "}
+      <span>
+        {state.city} {state.street}
+      </span>
+      <p>
+        <MdAlternateEmail font-size={36} className="icon_pink" />
+      </p>
     </div>
   );
 };
